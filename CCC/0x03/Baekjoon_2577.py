a16e4f0bf6c5cc7bdcf5b82fa97a4459534cde5d
hb9397 <hb9397@naver.com>
2023-09-04 10:07:05 +0900

    백준_숫자의 개수(미 풀이) https://www.acmicpc.net/problem/2577

--- CCC/0x03/Baekjoon_2577.py
+++ CCC/0x03/Baekjoon_2577.py
@@ -1,6 +1,11 @@
 # https://www.acmicpc.net/problem/2577
 numbers = [int(input()) for _ in range(3)]
-numbersCom = 0
+numbersCom = 1
 
 for item in numbers:
-    numbers
+    numbersCom *= item
+
+numbersCom = str(numbersCom)
+
+for i in range(10):
+    print(numbersCom.count(str(i)))
