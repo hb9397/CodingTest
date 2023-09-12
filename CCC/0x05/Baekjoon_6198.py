bb4a5df28490d79b06bacf2cdb3f48acd31066c0
hb9397 <hb9397@naver.com>
2023-09-12 16:33:50 +0900

    백준_탑 https://www.acmicpc.net/problem/6198()

--- CCC/0x05/Baekjoon_6198.py
+++ CCC/0x05/Baekjoon_6198.py
@@ -3,17 +3,23 @@ import sys
 
 N = int(sys.stdin.readline().rstrip())
 buildings = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
-answer = [0]*N
+chkStack = []
+canSeeBuildingSum = 0
 
 for i in range(N):
-    currentBuildingShortThen = 0
+    print(chkStack)
 
-    for j in range(i+1, N):
-        if buildings[j] < buildings[i]:
-            currentBuildingShortThen += 1
+    # 포인트는 chkStack 에 빌딩의 높이가 쌓인다는 것은 chkStack 의 가장 위에 존재하는 빌딩의 높이보다 작은 빌딩들이 온다는 것이고,
+    # pop 되어 없어지는 빌딩의 높이들은 현재 buildings[i] 보다 짧은 빌딩이기 때문에 관찰할 수 없는 빌딩이므로 제거하는 것이다.
 
-        if buildings[j] >= buildings[i]:
-            break
+    while len(chkStack) > 0 and chkStack[-1] <= buildings[i]:
+        chkStack.pop()
 
-    answer[i] = currentBuildingShortThen
-print(sum(answer))
+    chkStack.append(buildings[i])
+
+    # 이렇게 되면 현재 buildings[i]가 chkStack 에 들어가 있는 만큼 마치 내림차순으로 정렬되어 연속된 빌딩 들이 내림차순
+    # 순서로 빌딩을 관찰할 수 있는 것에 해당되므로 길이 - 1 해서 자신을 제외한다.
+    canSeeBuildingSum += len(chkStack) - 1
+
+
+print(canSeeBuildingSum)
